b697eaf7e57a43d621ab980bb418ab0e5205d5c1
Kartonagnick <CastleOfDreams@yandex.ru>
2021-02-06 00:04:46 +0300

    fix #1, framework

---
+++ test/include/test-stable.hpp
@@ -0,0 +1,26 @@
+
+#pragma once
+
+#ifndef dTEST_STABLE_USED_ 
+#define dTEST_STABLE_USED_ 1
+
+#ifndef STABLE_RELEASE
+    #error #include <test-stable.hpp> require #define STABLE_RELEASE
+#endif
+
+    #define dFORBID_ZERO_SIZE_ARRAY
+    #define INCLUDE_AUTO_GENERATED
+
+    // #define INCLUDE_STRESS_TESTS
+    #define INCLUDE_LONG_LONG_TESTS
+    #define INCLUDE_LONG_TESTS
+
+//==============================================================================
+//===== modern/classic =====================================||==================
+    #define TEST_MODERN                                     // ready!
+    #define TEST_CLASSIC                                    // ready!
+//==============================================================================
+//==============================================================================
+
+#endif // !dTEST_STABLE_USED_
+
